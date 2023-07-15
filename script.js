431c44034fd3d30811c76ea963091991d1273928
miglesiasghys <macaiglesiasghys@gmail.com>
2023-07-15 12:24:03 -0300

    avances en js

---
+++ script.js
@@ -0,0 +1,46 @@
+const textButton=document.getElementById('texto-btn');
+const imgButton=document.getElementById('imagen-btn')
+const modeButton=document.getElementById('modo-btn')
+
+const textAside=document.getElementById('aside-texto');
+const imgAside=document.getElementById('aside-imagen');
+const formatAside=document.getElementsByClassName('container-formato')
+
+textButton.addEventListener('click', ()=> hideImgAside())
+imgButton.addEventListener('click', ()=> hideTextAside())
+modeButton.addEventListener('click', ()=> changeMode())
+
+const hideImgAside=()=>{
+    imgAside.classList.add('hidden');
+    textAside.classList.remove('hidden')
+}
+
+const hideTextAside=()=>{
+    textAside.classList.add('hidden');
+    imgAside.classList.remove('hidden')
+}
+
+const changeMode=()=>{
+    formatAside[0].classList.toggle('dark-aside')
+    formatAside[1].classList.toggle('dark-aside')
+}
+
+
+
+//
+const cajaColor = document.getElementById('box-color')
+let colorElegido=''
+const inputColor=document.getElementById('input-color')
+
+const cambioDeColor = (color) =>{
+    cajaColor.style.backgroundColor= `${colorElegido}`
+}
+
+const obtenerColor=(e)=> {
+    colorElegido= e.target.value;
+    console.log(`El color es:`, e.target.value)
+}
+
+inputColor.addEventListener('input', (evento)=>obtenerColor(evento))
+
+inputColor.addEventListener('input', ()=>cambioDeColor(colorElegido))

6d9dc5748a4ffa2b1d99dcc70ae1270ed73818bc
miglesiasghys <macaiglesiasghys@gmail.com>
2023-07-16 22:45:47 -0300

    complete aside text, incorporacion de filtros en js

--- script.js
+++ script.js
@@ -25,34 +25,99 @@ const changeMode=()=>{
     formatAside[1].classList.toggle('dark-aside')
 }
 
-//
+// cambio de color del div img-meme
+
 const cajaColor = document.getElementById('box-color')
-let colorElegido=''
 const inputColor=document.getElementById('input-color')
+const colorName=document.getElementById('color-name')
 
-const cambioDeColor = (color) =>{
-    cajaColor.style.backgroundColor= `${colorElegido}`
+const cambioDeColor = (e) =>{
+    cajaColor.style.backgroundColor= `${e.target.value}`
+    colorName.textContent=`${inputColor.value}`
 }
 
-const obtenerColor=(e)=> {
-    colorElegido= e.target.value;
-    console.log(`El color es:`, e.target.value)
-}
+inputColor.addEventListener('input', (e)=>cambioDeColor(e))
 
-inputColor.addEventListener('input', (evento)=>obtenerColor(evento))
 
-inputColor.addEventListener('input', ()=>cambioDeColor(colorElegido))
 
-//
+// imagen del meme
+
 const imgMeme=document.getElementById('img-meme')
 const inputURL=document.getElementById('url-input')
 
 inputURL.addEventListener('input', (e)=>changeBackground(e))
 
 const changeBackground= (e) => {
-    console.log(e.target.value)
     imgMeme.style.backgroundImage=`url('${e.target.value}')`
-    // imgMeme.style.backgroundSize='cover'
-    // imgMeme.style.backgroundRepeat='no-repite'
-    // imgMeme.style.backgroundPosition='center'
+}
+
+// funcion Cambio de texto superior e inferior
+
+const topText=document.getElementById('top-text')
+const inputTop=document.getElementById('superior')
+
+inputTop.addEventListener('change', (e)=>changeTextTop(e))
+
+const changeTextTop= (e) => {
+    topText.textContent=`${e.target.value}`
+}
+
+const bottomText=document.getElementById('bottom-text')
+const inputBottom=document.getElementById('inferior')
+
+inputBottom.addEventListener('change', (e)=>changeTextBottom(e))
+
+const changeTextBottom= (e) => {
+    bottomText.textContent=`${e.target.value}`
+}
+
+//filtros
+
+const brightInput=document.getElementById('bright-input')
+const opacityInput=document.getElementById('opacity-input')
+const contrastInput=document.getElementById('contrast-input')
+const blurInput=document.getElementById('blur-input')
+const grayscaleInput=document.getElementById('grayscale-input')
+const sepiaInput=document.getElementById('sepia-input')
+const hueInput=document.getElementById('hue-input')
+const saturationInput=document.getElementById('saturation-input')
+const invertInput=document.getElementById('invert-input')
+
+const filtros = (e) => {
+    imgMeme.style.filter = 
+    `brightness(${brightInput.value})
+    opacity(${opacityInput.value})
+    contrast(${contrastInput.value}%)
+    blur(${blurInput.value}px)
+    grayscale(${grayscaleInput.value}%)
+    sepia(${sepiaInput.value}%)
+    saturate(${saturationInput.value}%)
+    invert(${invertInput.value})
+    hue-rotate(${hueInput.value}deg)`;
+};
+
+brightInput.addEventListener('input', (e)=>filtros(e))
+opacityInput.addEventListener('input', (e)=>filtros(e))
+contrastInput.addEventListener('input', (e)=>filtros(e))
+blurInput.addEventListener('input', (e)=>filtros(e))
+grayscaleInput.addEventListener('input', (e)=>filtros(e))
+sepiaInput.addEventListener('input', (e)=>filtros(e))
+hueInput.addEventListener('input', (e)=>filtros(e))
+saturationInput.addEventListener('input', (e)=>filtros(e))
+invertInput.addEventListener('input', (e)=>filtros(e))
+
+//
+
+
+
+//descargar
+const downloadBtn=document.getElementById('download')
+const finalMeme = document.getElementById("box-color");
+
+downloadBtn.addEventListener('click', () => downloadMeme());
+
+const downloadMeme = () => {
+    doToImage.toBlob(meme).then(function (blob) {
+    window.saveAs(blob, "mi-meme.png")
+})
 }
